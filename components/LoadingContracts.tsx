1d416a56a5957de984cfda8f725f7a0a835cda5f
myks-dapp <belvana20.pma@gmail.com>
2025-04-14 17:28:32 +0700

    Create LoadingContracts.tsx

---
+++ components/LoadingContracts.tsx
@@ -0,0 +1,7 @@
+export default function LoadingContracts() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-6 bg-white">
+      <p className="text-gray-500">Loading contracts...</p>
+    </div>
+  )
+}
