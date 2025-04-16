f7085f5c147a1dace389cb6ed03ad9e314637a7e
myks-dapp <belvana20.pma@gmail.com>
2025-04-16 08:16:38 +0700

    Create contracts.ts

---
+++ constants/contracts.ts
@@ -0,0 +1,7 @@
+// constants/contracts.ts
+
+// Alamat kontrak token MYKS di jaringan Polygon
+export const TOKEN_CONTRACT = '0xbae14e5a05030f6Bcff900Be3C02A260C96e5D6c';
+
+// Alamat kontrak penjualan token MYKS di jaringan Polygon
+export const SALE_CONTRACT = '0xdE36A031F39515E5A4D2700cbCc8837045667Dd7';
